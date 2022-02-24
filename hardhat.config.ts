573e1f52906d70b5245603ab556c14fbe4139ea8
Omar <omar@na2.com.br>
2022-02-24 17:57:13 -0300

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.11",
+  networks: {
+    rinkeby: {
+      url: process.env.RPC_URL_RINKEBY || "",
+      accounts:
+        process.env.PRIVATE_KEY_RINKEBY !== undefined
+          ? [process.env.PRIVATE_KEY_RINKEBY]
+          : [],
+    },
+    mainnet: {
+      url: process.env.RPC_URL_MAINNET || "",
+      accounts:
+        process.env.PRIVATE_KEY_MAINNET !== undefined
+          ? [process.env.PRIVATE_KEY_MAINNET]
+          : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
